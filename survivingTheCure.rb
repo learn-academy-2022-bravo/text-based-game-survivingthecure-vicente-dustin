353b0aae21cd01ac336f922c3acae21c52acca92
VicenteSaldana1 <vsaldana210@gmail.com>
2022-04-18 11:53:18 -0700

    completed pseudo code and foundation of game

---
+++ survivingTheCure.rb
@@ -0,0 +1,66 @@
+# Game Setup
+# Game story- 
+puts 
+puts 
+puts 'Welcome to Suriving The Cure'
+puts
+puts
+puts 'Its year 2055. Your entire town has been roaming the streets as the undead now.' 
+puts 
+puts 'Zombies are on the lose and it is your mission to make your way to a series of safehouses and through the hoard to the facility containing the cure.' 
+puts 
+puts 'Press [ENTER] to set up your player.' 
+ready = gets.chomp
+puts 'Enter your player name.'
+player_name = gets.chomp 
+# puts 'Select your weapon of choice.'
+# puts 
+# puts '1) Knife' 
+# puts '2) Shotgun'
+# puts '3) Rocket Launcher'
+# puts '4) Bat'
+# puts '5) Handgun'
+puts 
+puts 
+
+puts "Hello #{player_name}, you are ready to start your mission!" 
+puts 
+puts 'Press [ENTER] to begin.'
+ready = gets.chomp
+puts 
+puts 
+
+# Level 1
+# You decide the time is now to leave your house, you look left, and see a mass of zombies walking your way. 
+
+# Question1- 
+# With your weapon of choice, 
+# 1- you choose to engage the zombies in your attempt to pass them. 
+# 2- your quietly attempt to make your way around the zombies.
+
+# if statements depending on user choice 
+# option 1 return: "#{name} clashed head on against the zombies with his #{weapon} and defeated all the zombies, however, #name was injured in the attack and suffered serious wounds to his leg."
+
+# option 2: "You slowly creeped your way past the zombies and made it to the first safehouse."
+
+# Level 2
+# At the safehouse, you meet a group of rugged people who seem to be traveling together. You have rested and healed your injuries and are ready to move to the next safehouse. The group who you met at this safehouse has a different idea on how to get to the next safehouse. Option 1 - Do you join and trust the group to continue the mission with them? 
+# Option 2- Eat their food and continue the mission on your own. 
+
+# if statements depending on user choice
+# Option 1- Some members of the group were quite noisy and immediately attarcted a hoard. You all run to a dead-end alley with one latter. You are first up the latter and begin to pull the next person up when the hoard catches up and devours the group alive. You must continue your travel on your own. As you arrive at the next safehouse you think to yourself, should I have ate that food? 
+
+# option 2- You stuffed your stomach with food and sluggishly continue on your journey. En route to the next safehouse, a zombie surprised you and lashed on to your ankle with its teeth. With your #weapon, you kill the zombie and gruesomely amputate your foot and arrive safely at the safehouse. 
+
+# Level 3
+# From the window of this final safehouse, you can see the facility that holds the cure at the top of the hill in front of you. Between you lies a hoard of zombies surrounding and closing in on your neighbor's newborn baby. You know face your toughest decision of the journey. 
+# option 1- Quietly pass the unsuspecting zombies and consider the newborn baby as bait for your escape to the cure facility. 
+
+# option 2- Attempt to safe the newborn baby and make your escape to the cure facility. 
+
+# if statements depending on user choice
+# option 1- Your plan to use the newborn baby as bait as genius! Mission accopmlished, you survived the cure! (Your momma didn't raise no dummy!)
+
+# option 2- In your attempt to save the baby, the zombies were alerted to your presence. You make a mad dash for the baby, you scoop it up, tuck it in close to your chest, and sprint towards the cure facility when you sense the excruciating pain of a baby zombie biting down on your neck. Game over.   
+
+# Game Over
